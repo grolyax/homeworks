803c1b19e3754313a9bb5d7f3147a656e024de82
Olga Gromyko <olgagromyko@MacBook-Air-Olga.local>
2020-11-23 17:46:40 +0300

    add folder 19.11.2020

---
+++ 19.11.2020/script.js
@@ -0,0 +1,47 @@
+// Исправьте синтаксические ошибки
+
+function addNumbers(a, b, c) {
+    return a + b + c;
+  }
+  
+  function introduceMe(name, age) {
+    return "Привет, меня зовут" + " " + name + " " + "и мне" + " " + age + " " + "года";
+  }
+  
+  function getTotal(a, b) {
+    total = a + b;
+  
+    return "The total is" + " " + total;
+  }
+
+  
+  // Допишите функцию так, чтобы теперь она принимала параметры
+function multiply(x, y) {
+    return x * y;
+  }
+  
+  // функция должна возвращать произведение двух чисел
+  var result = multiply(3, 4);
+  
+  console.log(result);
+
+  // Напишите функцию divide которая получает частное двух переданных ей чисел
+
+  function divide(x, y) {
+    return x / y;
+  }
+  
+    var result = divide(3, 4);
+
+    console.log(result);
+
+    // Напишите функцию sum которая складывает два числа
+
+// Вызовите функцию и запишите ее возвращаемое значение в переменную sum
+function sum(x, y) {
+    return x + y;
+  }
+  
+    var sum = sum(3, 4);
+
+console.log(sum);
