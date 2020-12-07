3b2b0f16a62b7f3ff3a25eacb7f425768d703e1f
Olga Gromyko <olgagromyko@MacBook-Air-Olga.local>
2020-12-07 23:01:59 +0300

    fixed 26.11.

--- 26.11.2020./task/script.js
+++ 26.11.2020./task/script.js
@@ -1,9 +1,9 @@
-    var N;
+
 function fill(N, value) {
     var arr = [];
     for (i = 0; i < N-1; i++) {
         arr[i] = value;
-        arr.push(arr[i]);
+        arr.push(value);
     }
     return arr;
 }
@@ -11,8 +11,6 @@ function fill(N, value) {
 console.log('first');
 console.log(fill(4, 8));
 
-
-var array = [1, 2, 3, 4, 5];
 
 function reverse(array) {
     var result = [];
@@ -29,12 +27,7 @@ console.log(reverse([1, 2, 3, 4, 5]));
 function compact(arr) {
  var result = [];
     for (var i = 0; i < arr.length; i++){
-        if (arr[i] !== undefined
-             && arr[i] !== false 
-             && arr[i] !== '' 
-             && arr[i] !== 0 
-             && arr[i] !== null) {
-       
+        if (arr[i]) {
             result.push(arr[i]);
         }
     }
@@ -44,9 +37,6 @@ function compact(arr) {
 console.log('third');
 console.log(compact([1, 2, null, 0, 7, 'apple', undefined, true, false, '']));
 
-
-var array1 = [1, 2, 3, 4];
-var array2 = [1, 2, 3, 4];
 
 function isEqual(array1, array2) {
     if (!Array.isArray(array1) || !Array.isArray(array2)) {
@@ -69,9 +59,6 @@ function isEqual(array1, array2) {
 
 console.log('forth');
 console.log(isEqual([1, 2, 3, 4], [1, 2, 3, 4]));
-
-var arr1 = [5, 8, 9, 3];
-var arr2 = [8, 1, 2, 5, 4, 0];
 
 function intersection(arr1, arr2) {
     var result = [];
